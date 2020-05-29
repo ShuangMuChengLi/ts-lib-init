1fe57780c3d02d5f17136da3ad73052a82f14f09
林超群 <5671570+lin_chao_qun@user.noreply.gitee.com>
2020-05-29 16:38:38 +0800

    init

--- test/base.test.ts
+++ test/base.test.ts
@@ -1,35 +1,6 @@
-import rkAxios from '../src/index'
+import val from '../src/index'
 describe('rkAxios.get', () => {
-  test('get', async () => {
-    return await rkAxios.get(
-      'https://www.linchaoqun.com/html/cms/content.jsp',
-      {
-        menu : 'index',
-        id: '02b84ae2-b29e-46e6-8b06-2db5c99d3772'
-      }
-      ).then((res)=>{
-        return res
-    }).catch((e)=>{
-      throw e
-    })
-  })
-  test('setBeforeRequire', (done) => {
-    let BeforeRequireCalled = false
-    rkAxios.setBeforeRequire(( options, data)=>{
-      BeforeRequireCalled = true
-      return {data, options}
-    })
-    rkAxios.get('https://www.linchaoqun.com/html/cms/content.jsp').then(()=>{
-      if(BeforeRequireCalled){
-        done()
-      }
-    })
-  })
-
-  test('setErrorCallback', (done) => {
-    rkAxios.setErrorCallback(()=>{
-      done()
-    })
-    rkAxios.get('https://1')
+  test('get', () => {
+    expect(val).toEqual('init')
   })
 })
